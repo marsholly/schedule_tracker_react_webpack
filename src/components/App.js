6b0676c796c6d19fc52da0c65afdc942893f4384
HONG ZHOU <holly_zhou@yahoo.com>
2016-09-21 23:25:51 -0700

    finish MVP

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,81 @@
 const React = require('react');
 const NewSchedule = require('./NewSchedule');
+const ScheduleList = require('./ScheduleList');
 const { MuiThemeProvider } = require('material-ui');
+const moment = require('moment');
 
 
 const App = React.createClass({
   getInitialState() {
     return {
-      schedule: []
+      schedules: []
     }
   },
 
+  shouldComponentUpdate(nextProps, nextState) {
+    let nextSchedules = nextState.schedules;
+    let newScheduleOrder = nextSchedules.sort((cur, next) => {
+      let a = moment(cur.startTime).format('lll');
+      let b = moment(next.startTime).format('lll');
+      let curT = new Date(a);
+      let nextT = new Date(b);
+      return ( curT - nextT );
+    });
+    this.setState({ schedules: newScheduleOrder });
+    return true;
+  },
+
+  componentDidMount() {
+    let scheduleStore = this.scheduleStorage();
+    this.setState({ schedules: scheduleStore });
+  },
+
   createNewSchedule(newSchedule) {
-    console.log('newSchedule:', newSchedule)
+    let { schedules } = this.state;
+    this.setState({
+      schedules: [...schedules, newSchedule]
+    });
+
+    let scheduleStore = this.scheduleStorage();
+    scheduleStore.push(newSchedule);
+    this.writeToStorage(scheduleStore);
+  },
+
+  scheduleStorage() {
+    let json = localStorage.scheduleStore;
+    let scheduleStore;
+    try {
+      scheduleStore = JSON.parse(json);
+    } catch(e) {
+      scheduleStore = [];
+    }
+    return scheduleStore;
+  },
+
+  writeToStorage(scheduleStore) {
+    localStorage.scheduleStore = JSON.stringify(scheduleStore);
+  },
+
+  deleteSchedule(id) {
+    let scheduleStore = this.scheduleStorage();
+    scheduleStore = scheduleStore.filter(schedule => schedule.id !== id);
+    this.writeToStorage(scheduleStore);
+
+    let { schedules } = this.state;
+    this.setState({ schedules: scheduleStore });
+  },
+
+  updateSchedule(editSchedule) {
+    let json = localStorage.scheduleStore;
+    let scheduleStore = JSON.parse(json);
+    let index = scheduleStore.findIndex(schedule => {
+      return schedule.id === editSchedule.id;
+    });
+    scheduleStore[index] = editSchedule;
+    localStorage.scheduleStore = JSON.stringify(scheduleStore);
+    this.setState({
+      schedules: scheduleStore
+    })
   },
 
   render() {
@@ -20,6 +84,7 @@ const App = React.createClass({
         <div className="container">
           <h1 className="text-center">Schedule Tracker</h1>
           <NewSchedule createNewSchedule={this.createNewSchedule}/>
+          <ScheduleList schedules={this.state.schedules} deleteSchedule={this.deleteSchedule} updateSchedule={this.updateSchedule}/>
         </div>
       </MuiThemeProvider>
     )
---
+++ src/components/ScheduleList.js
@@ -0,0 +1,142 @@
+const React = require('react');
+const { TextField, TimePicker, RaisedButton, FlatButton, Dialog} = require('material-ui');
+const moment = require('moment');
+
+const style = {
+  margin: 12,
+};
+
+const ScheduleList = React.createClass({
+  getInitialState() {
+    return {
+      open: false,
+      editTitle:'',
+      editStartTime: null,
+      editEndTime: null,
+      editId:''
+    }
+  },
+
+  deleteRow(id) {
+    this.props.deleteSchedule(id);
+  },
+
+  editRow(schedule) {
+    this.setState({ open: true });
+    this.setState({
+      editTitle: schedule.title,
+      editId: schedule.id
+    });
+  },
+
+  saveEdit() {
+    let editSchedule = {
+      title: this.state.editTitle,
+      startTime: this.state.editStartTime,
+      endTime: this.state.editEndTime,
+      id: this.state.editId
+    };
+    this.props.updateSchedule(editSchedule);
+    this.setState({editId: ''});
+    this.handleClose();
+  },
+
+  editStartTimeChange(e, date) {
+    this.setState({ editStartTime: date });
+  },
+
+  editEndTimeChange(e, date) {
+    this.setState({ editEndTime: date });
+  },
+
+  handleClose() {
+    this.setState({ open: false });
+  },
+
+  render() {
+    let { schedules } = this.props;
+    let rows = schedules.map(schedule => {
+      return (
+        <tr key={schedule.id}>
+          <td>{schedule.title}</td>
+          <td>{moment(schedule.startTime).format('lll')}</td>
+          <td>{moment(schedule.endTime).format('lll')}</td>
+          <td>
+            <button className="btn btn-warning btn-xs" onClick={() => this.editRow(schedule)}>
+              <i className="glyphicon glyphicon-pencil"></i>
+            </button>
+          </td>
+          <td>
+            <button className="btn btn-danger btn-xs" onClick={() => this.deleteRow(schedule.id)}>
+              <i className="glyphicon glyphicon-trash"></i>
+            </button>
+          </td>
+        </tr>
+      )
+    });
+
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleClose}
+      />,
+      <FlatButton
+        label="Edit"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this.saveEdit}
+      />,
+    ];
+
+    return (
+      <div>
+        <table className="table table-striped">
+          <thead>
+            <tr className="success">
+              <th>Title</th>
+              <th>Start Time</th>
+              <th>End Time</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {rows}
+          </tbody>
+        </table>
+        <Dialog
+          title="Edit Schedule"
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+          <TextField
+            hintText="Title"
+            errorText="This field is required"
+            floatingLabelText="Schedule Title"
+            value = {this.state.editTitle}
+            onChange = {e => { this.setState({editTitle: e.target.value}) }}
+          />
+          <TimePicker
+            format="24hr"
+            hintText="24hr Format"
+            floatingLabelText="Start Time"
+            value={this.state.editStartTime}
+            onChange={this.editStartTimeChange}
+          />
+          <TimePicker
+            format="24hr"
+            hintText="24hr Format"
+            floatingLabelText="End Time"
+            value={this.state.editEndTime}
+            onChange={this.editEndTimeChange}
+          />
+        </Dialog>
+      </div>
+    )
+  }
+});
+
+module.exports = ScheduleList;
